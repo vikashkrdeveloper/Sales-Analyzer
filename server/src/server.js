a27e5d346d664510094cd42557a68b6ef5a0187d
VIKASH KUMAR <128849038+vikashkrdeveloper@users.noreply.github.com>
2024-06-20 13:58:03 +0530

    Sales Analyzer initial commit

--- server/src/server.js
+++ server/src/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: "*",
     credentials: true,
     methods: ['GET'],
 }));
